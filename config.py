1e6351f6e8ce7d5fa6a20f71258df55d935ecbf6
Adilson <andyadilon17@gmail.com>
2026-05-12 08:54:39 +0100

    Corrijindo conexão mysql

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 
 class Config:
-    MYSQL_HOST = 'mysql.railway.internal'
+    MYSQL_HOST = 'yamabiko.proxy.rlwy.net'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'MRnTzTcAnMNPlRJOmLKmmypdDkibwOPw'
     MYSQL_DB = 'railway'
+    MYSQL_PORT = '59054'
