0003c3e6a8148656016634e1327cc5bee95c1dbf
Adilson <andyadilon17@gmail.com>
2026-05-18 01:22:54 +0100

    Estilo

--- static/js/landing.js
+++ static/js/landing.js
@@ -132,3 +132,16 @@ setInterval(() => {
     createParticle();
 
 }, 300);
+
+
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector(".header");
+
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove("scrolled");
+    }
+
+});
